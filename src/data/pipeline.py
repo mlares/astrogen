2a7fe3885adc54629b0f5a2ea4de1b11461a68bf
mlares <marcelo.lares@gmail.com>
2021-10-01 16:39:21 -0300

    pipeline

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -105,6 +105,109 @@ def ft_low(s):
         y = s
     return y
 
+def re_names(string):
+    """
+    Dado un nombre, devuelve los nombres completos o iniciales
+    """
+    string = string.title()
+    regex1 = r"[A-Z][^A-Z\s]\w+"
+    fullnames = re.findall(regex1, string)
+    if len(fullnames)>0:
+        if '.' in fullnames[0]:
+            fullnames = ''
+    regex2 = "[A-Z][A-Z][A-Z]\s|[A-Z][A-Z]\s|[A-Z]\.|[A-Z]$"
+    regaux = r"[A-Z][A-Z][A-Z]|[A-Z][A-Z]"
+    laux = re.findall(regaux, string)
+    if len(laux)>0:
+        iniciales = ('. '.join(list(laux[0])) + '.').split()
+    else:
+        iniciales = re.findall(regex2, string)
+    if len(fullnames)>0 and len(iniciales)==0:
+        for s in fullnames:
+            iniciales.append(getinitials(s))
+    return ' '.join(fullnames), ' '.join(iniciales)
+
+def aut_compare(aut1, aut2):
+    """
+    each author: ap_full, ap_inic, nom_full, nom_inic
+    e.g.:
+    aut_compare([['Lares'], [], [], ['M.']], [['Lares'], [], [], ['M. E.']])
+    """
+    # comparar apellidos:
+    a1 = aut1[0]
+    a2 = aut2[0]
+
+    d_ap = difflib.SequenceMatcher(None, a1, a2).ratio()
+    d1_ap = jellyfish.damerau_levenshtein_distance(a1, a2)
+    d2_ap = jellyfish.jaro_distance(a1, a2)     
+    d3_ap = jellyfish.levenshtein_distance(a1, a2)                          
+
+    # comparar nombres:
+    if len(aut2[2])>0: # tiene nombre completo
+        a1 = aut1[2]
+        a2 = aut2[2]
+        d_n = difflib.SequenceMatcher(None, a1, a2).ratio()
+        d1_n = jellyfish.damerau_levenshtein_distance(a1, a2)
+        d2_n = jellyfish.jaro_distance(a1, a2)     
+        d3_n = jellyfish.levenshtein_distance(a1, a2)                          
+
+    else: # tiene solo iniciales
+        a1 = aut1[3]
+        a2 = aut2[3]
+        d_n = difflib.SequenceMatcher(None, a1, a2).ratio()
+        d1_n = jellyfish.damerau_levenshtein_distance(a1, a2)
+        d2_n = jellyfish.jaro_distance(a1, a2)     
+        d3_n = jellyfish.levenshtein_distance(a1, a2)                          
+
+    return [d_ap, d_n, d1_ap, d1_n, d2_ap, d2_n, d3_ap, d3_n,
+           len(aut1[0]), len(aut2[0]), 
+           len(aut1[1]), len(aut2[1])]
+
+def authmatch(x, ip, show=False):
+    """
+    Dado un autor y un paper, determinar si ese paper
+    es de ese autor.
+    0)ap_full, 1)ap_inic, 2)nom_full, 3)nom_inic,  <--- autor buscado
+    4)etal_ap_full, 5)etal_ap_inic, 6)etal_nom_full, 7)etal_nom_inic
+
+    """
+    nms = [[[]]*4, [[]]*4]
+    nms[0][0:2] = re_names(x.apellido)
+    nms[0][2:4] = re_names(x.nombre)
+
+    mx = -99
+
+    for au, af in zip(ip.author, ip.aff):
+        nl = au.split(',')
+        if len(nl)==2:
+            au_surname, au_name = nl
+        elif len(nl)>2:
+            au_surname = nl[0]
+            au_name = ''.join(nl[1:])
+        else:
+            au_surname = nl[0]
+            au_name = ''
+
+        nms[1][0:2] = re_names(au_surname)
+        nms[1][2:4] = re_names(au_name)
+        ll = aut_compare(*nms)
+
+        m = ll[0]+ll[1]-ll[2]-ll[3]-ll[4]-ll[5]
+        if m > mx:
+            lmx = ll
+            mx = m
+        ar = 1 if 'entina' in af else 0
+        if not ar: ar = 0.5 if (af==' ' or af=='-') else 0
+        ll.append(ar)
+        if show:
+            if ll[0]>0.8:
+                print(f'\u001b[46;1m   {au} \u001b[0m{af[:80]}')
+            else:
+                print(f'   \u001b[46;1m{au} \u001b[0m{af[:80]}')
+
+    return lmx
+         
+
 
 
 ## steps ##
@@ -722,7 +825,53 @@ def S04_pub_journal_index(*args):
 
 
 
+
     yield D
+
+
+
+def S04_pub_clean_papers(*args):
+    D = args[0]
+
+    # CARGAR MODELO :::::::::::::::::::::::::::::::::::::::::
+    clf, scaler = load('SVM_model_pars.joblib')
+                            
+
+    # FILTRAR: calcular el filtro :::::::::::::::::::::::::
+    lst = D.index
+    apin = []
+    for i in tqdm(lst): # LISTA DE AUTORES
+        x = D.loc[i]
+        saut = f'{x.apellido} {x.nombre}'
+        ipin = []
+        for ip in papers[i]:  # LISTA DE PAPERS
+            ll = authmatch(x, ip)
+            tst = np.array(ll[:6]).reshape(1, -1)
+            tst = scaler.transform(tst)
+            ipin.append(clf.predict(tst)[0])
+        apin.append(ipin)
+
+    fileD = '../../data/pickles/filter_saved.pk'
+    with open(fileD, 'wb') as f:
+       pickle.dump(apin, f)
+
+    with open(fileD, 'rb') as f:
+       apin = pickle.load(f)
+
+
+
+    # FILTRAR: eliminar papers :::::::::::::::::::::::::
+    for i in range(N):
+        aux = [papers[i][k] for k in range(len(apin[i])) if apin[i][k]]
+        papers[i] = aux.copy()
+
+    fileD = '../../data/pickles/papers_cleaned.pk'
+    with open(fileD, 'wb') as f:
+       pickle.dump(papers, f)     
+
+    yield D
+
+
 
 def S04_pub_value_added(*args):
     D = args[0]
@@ -791,10 +940,10 @@ def data_pipeline(**options):
     graph.add_chain(S01_read_aaa_table,
                     S02_add_OAC_data,
                     S02_add_IATE_data,
-                    #S02_add_IALP_data,
-                    #S02_add_GAE_data,
-                    #S02_add_IAFE_data,
-                    #S02_add_ICATE_data,
+                    S02_add_IALP_data,
+                    S02_add_GAE_data,
+                    S02_add_IAFE_data,
+                    S02_add_ICATE_data,
                     S02_add_CIC_data,
                     #
                     S03_add_gender,
@@ -815,21 +964,23 @@ def data_pipeline(**options):
 Como se hace esto???
 
 D = S01_read_aaa_table()
-D = S02_add_OAC_data(D)
-D = S02_add_IATE_data(D)
-D = S02_add_IALP_data(D)
-D = S02_add_GAE_data(D)
-D = S02_add_IAFE_data(D)
-D = S02_add_ICATE_data(D)
-D = S02_add_CIC_data(D)
-D = S03_add_gender(D)
-D = S03_add_age(D)
-D = S03_clean_and_sort(D)
-D = S04_pub_get_ads_entries(D)
-D = S04_pub_get_orcids(D)
-D = S04_pub_journal_index(D)
-D = S04_pub_value_added(D)
-load) 
+D = S02_add_OAC_data(next(D))
+D = S02_add_IATE_data(next(D))
+D = S02_add_IALP_data(next(D))
+D = S02_add_GAE_data(next(D))
+D = S02_add_IAFE_data(next(D))
+D = S02_add_ICATE_data(next(D))
+D = S02_add_CIC_data(next(D))
+D = S03_add_gender(next(D))
+D = S03_add_age(next(D))
+D = S03_clean_and_sort(next(D))
+D = S04_pub_get_ads_entries(next(D))
+D = S04_pub_get_orcids(next(D))
+D = S04_pub_journal_index(next(D))
+D = S04_pub_value_added(next(D)) 
+
+se puede interrumppir en cualquier lado y hacer D=next(D)
+
 """
 
 
